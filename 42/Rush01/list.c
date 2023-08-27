92701ea82378424c58a67a683948ff9347e25863
Daniel Barbosa Alves <danbarbo@c2r10p4.42sp.org.br>
2023-08-27 19:00:22 -0300

    fazendo coisas

--- 42/Rush01/list.c
+++ 42/Rush01/list.c
@@ -1,71 +1,91 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: victor-g <victor-g@student.42malaga.com>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/06 15:13:00 by victor-g          #+#    #+#             */
-/*   Updated: 2023/07/06 15:20:20 by victor-g         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   list.c                                             :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2023/08/27 16:05:17 by danbarbo          #+#    #+#             */
+// /*   Updated: 2023/08/27 16:05:21 by danbarbo         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include <stdlib.h>
+// #include <stdlib.h>
 
-struct	s_tuple
-{
-	int	n1;
-	int	n2;
-	int	n3;
-	int	n4;
-};
+// struct	s_tuple
+// {
+// 	int	n1;
+// 	int	n2;
+// 	int	n3;
+// 	int	n4;
+// };
 
-struct	s_tuple_head
-{
-	int	count;
-	struct s_tuple	*list;
-};
+// // struct	s_tuple_head
+// // {
+// // 	int				count;
+// // 	struct s_tuple	*list;
+// // };
 
-struct s_tuple_head	*create_list(int size)
-{
-	struct s_tuple_head	*head;
-	struct s_tuple	*list;
+// struct	s_perm_head
+// {
+// 	int				count;
+// 	struct s_tuple	*list;
+// };
 
-	head = (struct s_tuple_head *) malloc(sizeof(struct s_tuple_head) * size);
-	list = (struct s_tuple *) malloc(sizeof(struct s_tuple) * size);
-	head->count = 0;
-	head->list = list;
-	return (head);
-}
+// struct s_tuple	*create_table(int size)
+// {
+// 	struct s_tuple	*list;
 
-void	delete(int *list)
-{
-	free(list);
-}
+// 	list = (struct s_tuple *) malloc(sizeof(struct s_tuple) * size * size);
+// 	return (list);
+// }
 
-struct s_tuple	get_value(struct s_tuple_head *list, int index)
-{
-	struct s_tuple	*list_vector;
-	struct s_tuple	item;
+// /*
+// 	size só para 4, que fica 24
+// 	para maiores deve fazer a conta da permutação (n!)
+// */
+// struct s_perm_head	*create_permutation_list(int size)
+// {
+// 	int					size_perm;
+// 	struct s_tuple		*list;
+// 	struct s_perm_head	*perm;
 
-	list_vector = list->list;
-	item = list_vector[index];
-	return (item);
-}
+// 	size_perm = size;
+// 	while (--size > 0)
+// 		size_perm *= size;
+// 	perm = (struct s_perm_head *) malloc(sizeof(struct s_perm_head));
+// 	list = (struct s_tuple *) malloc(sizeof(struct s_tuple) * size_perm);
+// 	return (list);
+// }
 
-void	set_value(struct s_tuple_head *list, struct s_tuple item, int index)
-{
-	struct s_tuple	*list_vector;
+// void	delete(int *list)
+// {
+// 	free(list);
+// }
 
-	list_vector = list->list;
-	list_vector[index] = item;
-}
+// struct s_tuple	get_value(struct s_tuple_head *list, int index)
+// {
+// 	struct s_tuple	*list_vector;
+// 	struct s_tuple	item;
 
-void	add_value(struct s_tuple_head *list, struct s_tuple item)
-{
-	struct s_tuple	*list_vector;
+// 	list_vector = list->list;
+// 	item = list_vector[index];
+// 	return (item);
+// }
 
-	list_vector = list->list;
-	list_vector[list->count] = item;
-	list->count++;
-}
+// void	set_value(struct s_tuple_head *list, struct s_tuple item, int index)
+// {
+// 	struct s_tuple	*list_vector;
+
+// 	list_vector = list->list;
+// 	list_vector[index] = item;
+// }
+
+// void	add_value(struct s_tuple_head *list, struct s_tuple item)
+// {
+// 	struct s_tuple	*list_vector;
+
+// 	list_vector = list->list;
+// 	list_vector[list->count] = item;
+// 	list->count++;
+// }
--- 42/Rush01/main.c
+++ 42/Rush01/main.c
@@ -1,22 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 16:05:28 by danbarbo          #+#    #+#             */
+/*   Updated: 2023/08/27 18:30:15 by danbarbo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-#include "list.c"
+// #include "list.c"
+#include "table.h"
+#include "tuple.h"
+#include "permutations.h"
 
-int main() {
-    struct s_tuple_head *tupla = create_list(24);
+int main(int argv, char **argc) {
+    // struct s_tuple_head *tupla = create_list(24);
 
-    struct s_tuple item1 = {1, 2, 3, 4};
-    struct s_tuple item2 = {2, 1, 3, 1};
+    // struct s_tuple item1 = {1, 2, 3, 4};
+    // struct s_tuple item2 = {2, 1, 3, 1};
 
-    add_value(tupla, item1);
-    add_value(tupla, item2);
+    // add_value(tupla, item1);
+    // add_value(tupla, item2);
 
-    struct s_tuple item3 = get_value(tupla, 0);
-    struct s_tuple item4 = get_value(tupla, 1);
+    // struct s_tuple item3 = get_value(tupla, 0);
+    // struct s_tuple item4 = get_value(tupla, 1);
 
-    printf("%d %d %d %d\n", item3.n1, item3.n2, item3.n3, item3.n4);
-    printf("%d %d %d %d\n", item4.n1, item4.n2, item4.n3, item4.n4);
+    // printf("%d %d %d %d\n", item3.n1, item3.n2, item3.n3, item3.n4);
+    // printf("%d %d %d %d\n", item4.n1, item4.n2, item4.n3, item4.n4);
 
-    free(tupla);
+    // free(tupla);
+
+    // if (argv == 2) {
+    //     printf("%s\n", argc[0]);
+    //     printf("%s\n", argc[1]);
+    // }
+
+    struct s_perm   *permutations = create_perm(4);
+
+    struct s_tuple	ret;
+
+    char a[] = {"1234"};
+
+    permute(a, 0, 3, permutations);
+
+    for (int i = 0; i < 24; i++) {
+        ret = get_value_tuple(permutations, i);
+        printf("%d %d %d %d\n", ret.n1, ret.n2, ret.n3, ret.n4);
+    }
+
+    delete(permutations);
 
     return 0;
 }
